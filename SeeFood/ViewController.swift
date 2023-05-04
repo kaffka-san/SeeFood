3f87020e3624d7eae942e3ac519304a474b28b16
kaffka <kaffka.anastasia@gmail.com>
2023-05-04 16:28:59 +0200

    Create a simple app Hotdog or Not Hotdog using CoreML

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            ImageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CiImage")
+            }
+            detect(image: ciImage)
+        }
+        imagePicker.dismiss(animated: true)
+      
+    }
+    func detect(image: CIImage){
+ 
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML Model Failed")
+        }
+        let request = VNCoreMLRequest(model: model){
+            request, error in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to procceds")
+            }
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog!"
+                }
+                else{
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+       let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        } catch {
+            print("\(error)")
+        }
+      
+        
+    }
+    
+    @IBAction func CameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
